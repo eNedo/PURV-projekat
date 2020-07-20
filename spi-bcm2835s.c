00bdf2aafc7f09022f48eb862f120a70938076bf
eNedo <32650507+eNedo@users.noreply.github.com>
2020-07-20 20:55:05 +0200

    Interapt samo u soft dijelu

--- spi-bcm2835s.c
+++ spi-bcm2835s.c
@@ -31,7 +31,7 @@
 #include <linux/io.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/of.h>
+#include <linux/of.h> 
 #include <linux/of_address.h>
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
